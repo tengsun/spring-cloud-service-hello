86dbdfe6d8cd2379155c80a16d1903953bc60c91
tengsun <tengsun@cn.ibm.com>
2018-09-14 10:28:24 +0800

    refine hello service log message

--- src/main/java/st/demo/DemoServiceHelloApplication.java
+++ src/main/java/st/demo/DemoServiceHelloApplication.java
@@ -1,36 +1,29 @@
 package st.demo;
 
-import java.util.List;
+import java.util.logging.Logger;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @RestController
 public class DemoServiceHelloApplication {
+
+	private Logger logger = Logger.getLogger(getClass().getName());
 
 	@Value("${spring.application.name}")
 	private String serviceId;
 
-	@Autowired
-	private DiscoveryClient client;
-
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String hello() {
-		List<ServiceInstance> instances = client.getInstances(serviceId);
-		String clientInfo = "";
-		if (instances != null && instances.size() > 0) {
-			ServiceInstance instance = instances.get(0);
-			clientInfo = "Host: " + instance.getHost() + ", ServiceId: " + instance.getServiceId();
-		}
-		return "Hello, Spring Cloud! " + clientInfo;
+		logger.info("Got a request from client.");
+		return "Hello, Spring Cloud!";
 	}
 
 	public static void main(String[] args) {
